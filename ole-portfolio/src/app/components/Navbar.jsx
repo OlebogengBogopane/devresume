2bb49fcbdd02970a89446a13ba6e8f71494f71bc
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-05-16 14:36:04 +0200

    Navbar

--- ole-portfolio/src/app/components/Navbar.jsx
+++ ole-portfolio/src/app/components/Navbar.jsx
@@ -1,19 +1,44 @@
 import Link from 'next/link'
 import React from 'react'
+import NavLink from './NavLink'
+
+const navLinks = [
+    {
+     title: "About",
+     path:"#about"   
+    },
+    {
+      title:"Projects",
+      path:"#projects"
+    },
+    {
+        title:"Contact",
+        path:"#contact"
+    }
+]
 
 const Navbar = () => {
   return (
-    <nav>
-        <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
+    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#6aab7e4f] bg-opacity-90'>
+        <div className='flex flex-wrap items-center justify-between mx-auto px-4'>
             <Link href={"/"} className=''>
                  
-                    <img src="/images/logo.png" alt="logo"
+                    <img className='sm:h-20 ' src="/images/logo.png" alt="logo"
                     width={200}
                     height={200}
                      />
-                 
                  </Link>
-            <div className='menu'></div>
+            <div className='menu  md:block md:w-auto' id='navbar'>
+                <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
+                {
+                    navLinks.map((link, index) => (
+                        <li ke={index}>
+                            <NavLink href={link.path} title={link.title}/>
+                        </li>
+                    ))
+                }
+                </ul>
+            </div>
         </div>
     </nav>
   )
