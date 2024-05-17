b743d41657b520417aba00c10b806858dc55cc33
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-05-17 15:08:35 +0200

    burger menu open/close state

--- ole-portfolio/src/app/components/Navbar.jsx
+++ ole-portfolio/src/app/components/Navbar.jsx
@@ -1,6 +1,8 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import NavLink from './NavLink'
+
 
 const navLinks = [
     {
@@ -18,6 +20,7 @@ const navLinks = [
 ]
 
 const Navbar = () => {
+    const [navbarOpen,setNavbarOpen] = useState(false);
   return (
     <nav className='fixed top-0 left-0 right-0 z-10 bg-[#6aab7e4f] bg-opacity-90'>
         <div className='flex flex-wrap items-center justify-between mx-auto px-4'>
@@ -28,7 +31,23 @@ const Navbar = () => {
                     height={200}
                      />
                  </Link>
-            <div className='menu  md:block md:w-auto' id='navbar'>
+                 <div className='mobile-menu block md:hidden'>
+                    {!navbarOpen ? (
+                    <button 
+                    onClick={()=> setNavbarOpen(true)}
+                    className='text-slate-200 flex items-center px-3 py-3 border rounded border-slate-200 hover:bg-slate-500'>
+                        <img src='/images/burger-men.png' alt='menu' className='w-5 h-5' />
+                        </button>
+                    ):(
+                        <button 
+                        onClick={()=> setNavbarOpen(false)}
+                        className='text-slate-200 flex items-center px-2 py-2 border rounded border-slate-200 hover:bg-slate-800'>
+                        <img src='/images/close-men.png' alt='menu' className='w-5 h-5' />
+                        </button>
+                    )
+                    }
+                 </div>
+            <div className='menu hidden md:block md:w-auto' id='navbar'>
                 <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
                 {
                     navLinks.map((link, index) => (
