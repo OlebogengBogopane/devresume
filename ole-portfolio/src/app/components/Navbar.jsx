53ff037012385103e1ce18fa55ba1c761da8725e
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-05-21 08:47:43 +0200

    nav full opacity

--- ole-portfolio/src/app/components/Navbar.jsx
+++ ole-portfolio/src/app/components/Navbar.jsx
@@ -22,7 +22,7 @@ const navLinks = [
 const Navbar = () => {
     const [navbarOpen,setNavbarOpen] = useState(false);
   return (
-    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#6aab7e4f] bg-opacity-90'>
+    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#6aab7e] bg-opacity-100'>
         <div className='flex flex-wrap items-center justify-between mx-auto px-4'>
             <Link href={"/"} className=''>
                  
