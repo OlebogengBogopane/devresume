6ee743b274c7a32205f6841a193ab52864d97af2
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-06-05 14:51:10 +0200

    content bullet-list

--- ole-portfolio/src/app/components/AboutSection.jsx
+++ ole-portfolio/src/app/components/AboutSection.jsx
@@ -9,7 +9,7 @@ const TAB_DATA = [
     title: "Skills",
     id:"Skills",
     content :(
-    <ul>
+    <ul className='list-disc pl-2'>
       <li> Node.js </li>
       <li> Scrimba </li>
       <li> Next.js </li>
@@ -23,7 +23,7 @@ const TAB_DATA = [
     title: "Education",
     id:"Education",
     content :(
-    <ul>
+    <ul className='list-disc pl-2'>
       <li> Code Space Academy </li>
       <li> T-Systems SA</li>
       <li> Bafokeng High School </li>
@@ -35,7 +35,7 @@ const TAB_DATA = [
   title: "Certificates",
   id:"Certificates",
   content :(
-  <ul>
+  <ul className='list-disc pl-2'>
     <li> Code Space Academy </li>
     <li> Udemy</li>
     <li> CompTIA certification </li>
@@ -61,11 +61,11 @@ const AboutSection = () => {
     <section className=''>
         <div className=' md:grid  md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
             <Image src='/images/matrix-log.gif' alt='about' width={500} height={500}/>
-            <div>
+            <div className='mt-4 md:mt-0 text-left flex-col h-full'>
                 <h2 className='text-4xl font-bold mb-4 '>About Me</h2>
                 <p className='md:text-lg'> I am a Software developer graduate from CodeSpace academy with 1year experience ,learning html,css and javascript alongside javscript frameworks(React & NextJs). throughout the program I learned how to work in an Agile environment and deliver seamingless and up to par web applications.I work very well in teams and always look forward to gaining new skills and creative innovative applications for my community and the world at large
                 </p>
-                <div className='flex flex-row  mt-8 '>
+                <div className='flex flex-row justify-start mt-8 '>
                   <TabButton 
                   selectTab={()=> handleTabChange("Skills")} active={tab==="Skills"}> {""}Skills{""}
                    </TabButton>
