edbd8db0c9fcd6e2b4ca079e32b3f726a8974bab
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-06-06 14:20:10 +0200

    icons position center

--- ole-portfolio/src/app/components/ProjectCard.jsx
+++ ole-portfolio/src/app/components/ProjectCard.jsx
@@ -6,16 +6,29 @@ const ProjectCard = ( {imgUrl, title, description }) => {
   return (
     <div>
         <div className='h-52 md:h-72 rounded-t-xl relative group' 
-                style={{ background:`url(${imgUrl})`, backgroundSize:"cover"}}>
-                    <div className='overlay  items-center justify-center absolute top-0 left-0 w-full h-full bg-[#199a1988] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
-                        <Link href="/" className='h-14 w-14 border-2 rounded-full border-[#54ac71] hover:border-white'>
-                        <Image className='w-10 h-10  text-[#54ac71] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer '
+                style={{ background:`url(${imgUrl})`, backgroundSize:"cover"}}
+                >
+                    <div className='overlay flex items-center justify-center space-x-4 absolute top-0 left-0 w-full h-full bg-[#199a1988] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
+                        <Link href="/" className='h-14 w-14 border-2 rounded-full border-[#54ac71] hover:border-white flex items-center justify-center group/link'>
+                        <Image 
+                        className='w-10 h-10  text-[#54ac71]   cursor-pointer group-hover/link hover:text-white'
                         src="/images/view-icon.png"
-                        alt='eye'
-                        width={50}
-                        height={50}
+                        alt='view'
+                        width={40}
+                        height={40}
                         />
                         </Link>
+                        
+                    
+                        <Link href="/" className='h-14 w-14 border-2 rounded-full border-[#54ac71] hover:border-white flex items-center justify-center group/link'>
+                        <Image className='w-10 h-10  text-[#54ac71]  cursor-pointer group-hover/link hover:text-white'
+                        src="/images/eye-icon.png"
+                        alt='eye'
+                        width={40}
+                        height={40}
+                        />
+                        </Link>
+                        
                     </div>
                 </div>
                 <div className='text-green rounded-b-xl bg-[#181818] py-6 px-4'>
