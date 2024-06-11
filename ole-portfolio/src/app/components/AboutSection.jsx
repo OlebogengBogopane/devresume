d7f1c8675fb6b4e0a95f57cfd9fb16366aabf82e
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-06-11 14:23:25 +0200

    About green

--- ole-portfolio/src/app/components/AboutSection.jsx
+++ ole-portfolio/src/app/components/AboutSection.jsx
@@ -63,7 +63,7 @@ const AboutSection = () => {
         <div className=' md:grid  md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
             <Image src='/images/matrix-log.gif' alt='about' width={500} height={500}/>
             <div className='mt-4 md:mt-0 text-left flex-col h-full'>
-                <h2 className='text-4xl font-bold mb-4 '>About Me</h2>
+                <h2 className='text-4xl text-green-600 font-bold mb-4 '>About Me</h2>
                 <p className='md:text-lg'> I am a Software developer graduate from CodeSpace academy with 1year experience ,learning html,css and javascript alongside javscript frameworks(React & NextJs). throughout the program I learned how to work in an Agile environment and deliver seamingless and up to par web applications.I work very well in teams and always look forward to gaining new skills and creative innovative applications for my community and the world at large
                 </p>
                 <div className='flex flex-row justify-start mt-8 '>
