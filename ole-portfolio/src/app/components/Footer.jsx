7f70848e56e142268d5f7766c44915809af1b7b7
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-06-13 19:39:44 +0200

    footer-styling

--- ole-portfolio/src/app/components/Footer.jsx
+++ ole-portfolio/src/app/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer >
-        <div>
-        <span>OJarts</span>
+    <footer className='footer border border-top-[#33353F] border-l-transparent border-r-transparent'>
+        <div className='container p-12 flex justify-between'>
+        <span >OJarts</span>
         <p>All rights reserved</p>
         </div>
         </footer>
