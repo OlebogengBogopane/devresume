454f9affbda851ecc6f278e4457395e6efbfe56b
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-06-14 17:01:39 +0200

    footer

--- ole-portfolio/src/app/components/Footer.jsx
+++ ole-portfolio/src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='footer border border-t-[#59e0a378] border-l-transparent border-r-transparent'>
-        <div className='container p-12 flex justify-between'>
+        <div className='container p-12 flex justify-between ' >
         <span className='font-bold'>OJarts</span>
         <p className='text-green-400'>All rights reserved</p>
         </div>
