cff67ac2a5d82e0ef1c91d8240a6845578b8cbb5
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-06-18 01:29:17 +0200

    remove bg color

--- ole-portfolio/src/app/components/Footer.jsx
+++ ole-portfolio/src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='footer border border-t-[#59e0a378] border-l-transparent border-r-transparent bg-green-400'>
         <div className='container p-12 lg:p-12 flex justify-between ' >
-        <span className='font-bold text-green-400'>OJarts</span>
+        <span className='font-bold '>OJarts</span>
         <p className='text-green-400'>All rights reserved</p>
         </div>
         </footer>
