068158b607303dc61278f81c752fbbddab9a12fb
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-06-18 01:32:36 +0200

    remove  bg color

--- ole-portfolio/src/app/components/Footer.jsx
+++ ole-portfolio/src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='footer border border-t-[#59e0a378] border-l-transparent border-r-transparent bg-green-400'>
+    <footer className='footer border border-t-[#59e0a378] border-l-transparent border-r-transparent '>
         <div className='container p-12 lg:p-12 flex justify-between ' >
         <span className='font-bold '>OJarts</span>
         <p className='text-green-400'>All rights reserved</p>
