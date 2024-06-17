199591ce1ccfe286ef3ce721c7e1702f9218cbd3
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-06-18 00:00:00 +0200

    bg-green

--- ole-portfolio/src/app/components/Footer.jsx
+++ ole-portfolio/src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='footer border border-t-[#59e0a378] border-l-transparent border-r-transparent'>
+    <footer className='footer border border-t-[#59e0a378] border-l-transparent border-r-transparent bg-green-400'>
         <div className='container p-12 lg:p-12 flex justify-between ' >
         <span className='font-bold text-green-400'>OJarts</span>
         <p className='text-green-400'>All rights reserved</p>
