2218cf4b74954bd2cea5d9ee2798668649a618aa
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-06-17 23:52:49 +0200

    text-color green

--- ole-portfolio/src/app/components/Footer.jsx
+++ ole-portfolio/src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='footer border border-t-[#59e0a378] border-l-transparent border-r-transparent'>
         <div className='container p-12 flex justify-between ' >
-        <span className='font-bold'>OJarts</span>
+        <span className='font-bold text-green-400'>OJarts</span>
         <p className='text-green-400'>All rights reserved</p>
         </div>
         </footer>
