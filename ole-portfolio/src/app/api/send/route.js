5a13227274c1149e90da802e5476a68508ec6f84
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-06-20 09:41:06 +0200

    request and respond param

--- ole-portfolio/src/app/api/send/route.js
+++ ole-portfolio/src/app/api/send/route.js
@@ -5,7 +5,7 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST() {
+export async function POST(req, res) {
   try {
     const data = await resend.emails.send({
       from: fromEmail,
