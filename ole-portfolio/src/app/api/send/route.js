0601ead9bb4b3c1804499e2eb0a9346371698e47
Olebogeng Bogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2024-06-20 09:52:33 +0200

    p tag regards section

--- ole-portfolio/src/app/api/send/route.js
+++ ole-portfolio/src/app/api/send/route.js
@@ -16,7 +16,7 @@ export async function POST(req, res) {
       react: (
           <>
           <h1>{subject}</h1>
-  
+          <p> Thank you for contacting us!</p>
           <p>{message}</p>
           </>
     ),
